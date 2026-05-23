4bde708c1f4bb89e0cf91f8cfce9ad5ba4def91b
zlkdjkdj <kil07201@naver.com>
2026-05-23 09:06:47 +0900

    feat: 초기 프로젝트 구조 생성 및 설정 완료

---
+++ my-memory-backend/src/main/java/com/example/my_memory_backend/MyMemoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_memory_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMemoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMemoryBackendApplication.class, args);
+	}
+
+}
---
+++ my-memory-backend/src/test/java/com/example/my_memory_backend/MyMemoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_memory_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMemoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
