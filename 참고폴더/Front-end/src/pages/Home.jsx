0c3a6e77190580491dbd7e2d017aa204fda56b3c
zlkdjkdj <kil07201@naver.com>
2026-06-14 11:44:55 +0900

    chore: .gitignore에 참고폴더 추가 및 캐시 삭제

--- 참고폴더/Front-end/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1> 홈 페이지 </h1>
-    </>
-  );
-}
--- 참고폴더/basicProject/src/main/java/com/dongyang/basicProject/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dongyang.basicProject.repository;
-
-import com.dongyang.basicProject.entity.ArticleEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<ArticleEntity, Long > {
-
-}
